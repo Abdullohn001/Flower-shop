cf81146f0e6b05e041ec55e1ab0c3c6e25d48a5c
abdullohn001 <muhammadaligulomov41@gmail.com>
2024-09-12 12:45:34 +0500

    ready

--- src/components/AddNewItemModal.jsx
+++ src/components/AddNewItemModal.jsx
@@ -1,26 +1,7 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { getFormData } from "../lib/yutils/index";
-import { useAppStore } from "../lib/zustand";
-import SelectCategory from "./SelectCategory";
-import SelectColor from "./SelectColor";
-import { SelectCountry } from "./SelectCountry";
+import React from 'react'
 
 export default function AddNewItemModal() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const result = getFormData(e.target);
-    console.log(result);
-  };
-
-  const addItemModal = useAppStore((state) => state.addItemModal);
-  const setAddItemModal = useAppStore((state) => state.setAddItemModal);
-
-  return ;
+  return (
+    <div>AddNewItemModal</div>
+  )
 }
