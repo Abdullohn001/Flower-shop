9e5bcc542beb07a98d0a66040b80e52d061e093f
abdullohn001 <muhammadaligulomov41@gmail.com>
2024-09-13 11:05:57 +0500

    ready

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -1,14 +1,26 @@
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
 import { buttonVariants } from "@/components/ui/button";
-import { PlusCircleIcon } from "lucide-react";
+import { PlusCircleIcon, PlusIcon } from "lucide-react";
 import { uploadImage } from "../request";
+import { useRef, useState } from "react";
 
 export default function UploadImage() {
-    function handleGetImage(img) {
-        uploadImage(img)
+  const refImg = useRef(null);
+  const [img, setImg] = useState(null);
+  console.log(img)
+
+  function handleGetImage(img, type = "local") {
+    if (type === "url") {
+      setImg(img);
+    } else {
+      uploadImage(img)
+        .then((req) => setImg(req))
+        .catch((error) => console.log(error));
     }
+  }
   return (
     <div className="w-full">
       <Label className="ml-2">Rasim Yuklang..</Label>
@@ -28,22 +40,32 @@ export default function UploadImage() {
             >
               <PlusCircleIcon />
             </span>
-            <Input onChange={({target: {files}}) => handleGetImage(files[0])} className="sr-only"  type="file" />
+            <Input
+              onChange={({ target: { files } }) => handleGetImage(files[0])}
+              className="sr-only"
+              type="file"
+            />
           </Label>
         </TabsContent>
         <TabsContent value="url">
           <Label htmlFor="url" className="mb-1 ml-2">
             Havola*
           </Label>
-          <Input
-            id="url"
-            placeholder=" Rasimni havolasini kiriting"
-            className="w-full"
-            type="url"
-          />
+          <div className="flex w-full items-center gap-2">
+            <Input
+              ref={refImg}
+              id="url"
+              placeholder=" Rasimni havolasini kiriting"
+              className="w-full"
+              type="url"
+            />
+            <Button type="button" onClick={() => handleGetImage(refImg.current.value, "url")}>
+              <PlusIcon />
+            </Button>
+          </div>
         </TabsContent>
       </Tabs>
-
+      {img && <img className="mt-5" height="300px" width="100%" src={img} />}
     </div>
   );
 }
